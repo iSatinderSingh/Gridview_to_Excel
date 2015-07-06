998fe75bec5c3199d2f25261d7338232457aa67c
Satinder Singh <satindersinght@gmail.com>
2015-07-07 00:09:31 +0530

    Files added

---
+++ Gridview_to_excel/GridviewToExcel.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="GridviewToExcel.aspx.cs" Inherits="Gridview_to_excel.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Gridview to Excel</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="float;left;margin-left:70px">
+        <h2 style="color:#888;">Codepedia.info</h2>
+        <a href="http://codepedia.info/2015/07/export-gridview-data-to-excel-in-asp-net-c/" title="Detail Explaination How to Export gridview data to Excel in Asp.net C#">Click to view Details Explaination</a><br /><br />
+    <div>
+       
+<asp:GridView ID="GridView1" runat="server" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" CellPadding="4" ForeColor="#333333" GridLines="None">
+    <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+    <EditRowStyle BackColor="#999999" />
+    <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+    <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+    <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+    <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+    <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+    <SortedAscendingCellStyle BackColor="#E9E7E2" />
+    <SortedAscendingHeaderStyle BackColor="#506C8C" />
+    <SortedDescendingCellStyle BackColor="#FFFDF8" />
+    <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+</asp:GridView><br />
+<asp:Button ID="Button1" runat="server" OnClick="Button1_Click1" Text="Grid to CSV" />
+   
+    </div></div>
+    </form>
+</body>
+</html>
---
+++ Gridview_to_excel/GridviewToExcel.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Gridview_to_excel
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+protected void Page_Load(object sender, EventArgs e)
+{
+    if (!Page.IsPostBack)
+    {
+        GridView1.DataSource = getData();
+        GridView1.DataBind();
+    }
+}
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+       
+public DataTable getData()
+{
+
+    DataTable dt = new DataTable();
+    dt.Columns.Add("UserId", typeof(Int32));
+    dt.Columns.Add("UserName", typeof(string));
+    dt.Columns.Add("Education", typeof(string));
+    dt.Columns.Add("Location", typeof(string));
+    dt.Rows.Add(1, "Satinder Singh", "Bsc Com Sci", "Mumbai");
+    dt.Rows.Add(2, "Amit Sarna", "Mstr Com Sci", "Mumbai");
+    dt.Rows.Add(3, "Andrea Ely", "Bsc Bio-Chemistry", "Queensland");
+    dt.Rows.Add(4, "Leslie Mac", "MSC", "Town-ville");
+    dt.Rows.Add(5, "Vaibhav Adhyapak", "MBA", "New Delhi");
+    dt.Rows.Add(6, "Johny Dave", "MCA", "Texas");
+
+    return dt;
+}
+
+public void GridviewToExcel()
+{
+
+    StringBuilder builder = new StringBuilder();
+    string strFileName = "GridviewExcel_" + DateTime.Now.ToShortDateString() + ".csv";
+    builder.Append("Name ,Education,Location" + Environment.NewLine);
+    foreach (GridViewRow row in GridView1.Rows)
+    {
+        string name = row.Cells[0].Text;
+        string education = row.Cells[1].Text;
+        string location = row.Cells[2].Text;
+        builder.Append(name + "," + education + "," + location + Environment.NewLine);
+    }
+    Response.Clear();
+    Response.ContentType = "text/csv";
+    Response.AddHeader("Content-Disposition", "attachment;filename=" + strFileName);
+    Response.Write(builder.ToString());
+    Response.End();
+}
+
+protected void Button1_Click1(object sender, EventArgs e)
+{
+    GridviewToExcel();
+}
+
+        protected void btn_2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        public void ExportToExcel()
+        {
+            
+        }
+
+     }
+}
